3b5093495862456aad6eb3f187f9a4225beb071f
anastasiayurkina11 <106163636+anastasiayurkina11@users.noreply.github.com>
2023-11-13 17:41:38 +0300

    Update User.java

--- crud_3/src/main/java/net/javaguides/springboot/entity/User.java
+++ crud_3/src/main/java/net/javaguides/springboot/entity/User.java
@@ -13,21 +13,14 @@ public class User {
 	@Column(name = "first_name")
 	private String firstName;
 	
-	@Column(name = "last_name")
-	private String lastName;
-	
-	@Column(name = "email")
-	private String email;
 	
 	public User() {
 		
 	}
 	
-	public User(String firstName, String lastName, String email) {
+	public User(String firstName) {
 		super();
 		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
 	}
 	
 	public long getId() {
@@ -42,16 +35,5 @@ public class User {
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
 }
